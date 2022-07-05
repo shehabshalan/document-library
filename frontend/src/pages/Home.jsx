38e7e70cb07422dfdc7c51b5696a5c214ab89753
Shehab Shalan <shehab.shalan@hotmail.com>
2022-07-05 17:35:50 +0200

    expiration component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import UploadForm from "../components/UploadForm";
 import Documents from "../components/Documents";
 import { Container } from "@mui/material";
+import Expiration from "../components/Expiration";
 const Home = () => {
   return (
     <Container>
       <h1>Home</h1>
       <UploadForm />
       <Documents />
+      <Expiration />
     </Container>
   );
 };
