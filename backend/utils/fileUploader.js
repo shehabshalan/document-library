19267635a0aba174cbb749ef42cd45fb509476ec
Shehab Shalan <shehab.shalan@hotmail.com>
2022-07-06 01:16:03 +0200

    file uploader util

---
+++ backend/utils/fileUploader.js
@@ -0,0 +1,19 @@
+const cloudinary = require("cloudinary").v2;
+
+const checkFormat = require("./checkFormat");
+
+const fileUploader = async (file) => {
+  try {
+    const fileUploaded = await cloudinary.uploader.upload(file.path, {
+      folder: "library-files",
+      resource_type: "auto",
+      format: checkFormat(file.mimetype),
+    });
+    console.log(fileUploaded);
+    return fileUploaded;
+  } catch (err) {
+    console.log("Cloudinary error: ", err);
+  }
+};
+
+module.exports = fileUploader;
