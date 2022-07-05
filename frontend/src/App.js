1c31ae607c77d6735c8ff25a3c75b002c8cd537d
Shehab Shalan <shehab.shalan@hotmail.com>
2022-07-05 17:34:20 +0200

    share file route added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import UploadForm from "./components/UploadForm";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import ShareFile from "./pages/ShareFile";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/sharefile/:id" element={<ShareFile />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
