595189c9e9a57e42ae6532e68d5499b8d2f3b63a
Shehab Shalan <shehab.shalan@hotmail.com>
2022-07-05 17:33:25 +0200

    share file feature added

--- backend/controllers/shareFileController.js
+++ backend/controllers/shareFileController.js
@@ -1,20 +1,62 @@
 const ShareFile = require("../models/ShareFile");
+const File = require("../models/File");
+const moment = require("moment");
+
+const getSharedFiles = async (req, res) => {
+  try {
+    const sharedFiles = await ShareFile.find();
+    if (!sharedFiles)
+      return res.status(404).send({ message: "No files found" });
+    res.status(200).json(sharedFiles);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
 const getSharedFileById = async (req, res) => {
-  if (!req?.params?.id)
-    return res.status(400).json({ message: "ID is required" });
-  const id = req.params.id;
-  console.log(id);
-  const file = await ShareFile.findById(id);
-  if (!file) return res.status(201).json({ message: "file not found" });
-  res.json(file);
+  try {
+    if (!req?.params?.id)
+      return res.status(400).json({ message: "ID is required" });
+    if (!req.params.id.match(/^[0-9a-fA-F]{24}$/))
+      return res.status(400).json({ message: "ID is not valid" });
+    const id = req.params.id;
+    const sharedFile = await ShareFile.findById(id);
+    if (!sharedFile) return res.status(201).json({ message: "file not found" });
+
+    const now = moment().toDate();
+    const expiresAt = moment(sharedFile.expiresAt).toDate();
+    if (now > expiresAt) {
+      // await ShareFile.findByIdAndDelete(id);
+      return res.status(403).json({ message: "file expired" });
+    }
+
+    const file = await File.findById(sharedFile.fileId);
+    if (!file) return res.status(201).json({ message: "file not found" });
+
+    res.status(200).json({ message: file });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
 };
 
 const createSharedFile = async (req, res) => {
-  // to do - create shared file
+  try {
+    const { fileId, expiresAt } = req.body;
+    if (!fileId || !expiresAt)
+      return res
+        .status(400)
+        .json({ message: "File ID and expiration date are required" });
+    const newFile = await ShareFile.create({ fileId, expiresAt });
+    res.status(201).json({
+      message: `here is your link http://localhost:3000/sharefile/${newFile._id}`,
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 module.exports = {
   getSharedFileById,
   createSharedFile,
+  getSharedFiles,
 };
