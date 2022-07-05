72e5f292db97e6f85aa4846004f001f1a2939b19
Shehab Shalan <shehab.shalan@hotmail.com>
2022-07-05 17:33:10 +0200

    error handliing in files

--- backend/controllers/filesController.js
+++ backend/controllers/filesController.js
@@ -2,28 +2,42 @@ const File = require("../models/File");
 const cloudinary = require("cloudinary").v2;
 
 const getFiles = async (req, res) => {
-  const files = await File.find();
-  if (!files) return res.status(404).send({ message: "No files found" });
-  res.json(files);
+  try {
+    const files = await File.find();
+    if (!files) return res.status(404).send({ message: "No files found" });
+    res.json(files);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 const getFileById = async (req, res) => {
-  if (!req?.params?.id)
-    return res.status(400).json({ message: "ID is required" });
-  const id = req.params.id;
-  const file = await File.findById(id);
-  if (!file) return res.status(201).json({ message: "file not found" });
-  res.json(file);
+  try {
+    if (!req?.params?.id)
+      return res.status(400).json({ message: "ID is required" });
+    if (!req.params.id.match(/^[0-9a-fA-F]{24}$/))
+      return res.status(400).json({ message: "ID is not valid" });
+    const id = req.params.id;
+    const file = await File.findById(id);
+    if (!file) return res.status(201).json({ message: "file not found" });
+    res.json(file);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 // file uploads
 
 const fileUploader = async (path) => {
-  const fileUploaded = await cloudinary.uploader.upload(path, {
-    folder: "library-files",
-    resource_type: "auto",
-  });
-  return fileUploaded;
+  try {
+    const fileUploaded = await cloudinary.uploader.upload(path, {
+      folder: "library-files",
+      resource_type: "auto",
+    });
+    return fileUploaded;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const formatData = (files, filesUploaded) => {
@@ -61,17 +75,23 @@ const uploadFile = async (req, res) => {
 };
 
 const deleteFile = async (req, res) => {
-  if (!req?.params?.id)
-    return res.status(400).json({ message: "ID is required" });
-  const id = req.params.id;
-  const file = await File.findById(id);
-  if (!file) {
-    return res
-      .status(204)
-      .json({ message: `File ID ${req.params.id} not found` });
+  try {
+    if (!req?.params?.id)
+      return res.status(400).json({ message: "ID is required" });
+    if (!req.params.id.match(/^[0-9a-fA-F]{24}$/))
+      return res.status(400).json({ message: "ID is not valid" });
+    const id = req.params.id;
+    const file = await File.findById(id);
+    if (!file) {
+      return res
+        .status(204)
+        .json({ message: `File ID ${req.params.id} not found` });
+    }
+    const result = await file.remove();
+    res.json({ message: "File deleted" });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
   }
-  const result = await file.remove();
-  res.json({ message: "File deleted" });
 };
 
 module.exports = {
