356fd5977f512307b40428dd3c908daddee73c54
Shehab Shalan <shehab.shalan@hotmail.com>
2022-07-05 02:40:48 +0200

    organized home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,36 +1,14 @@
 import React from "react";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import UploadForm from "../components/UploadForm";
+import Documents from "../components/Documents";
+import { Container } from "@mui/material";
 const Home = () => {
-  const navigate = useNavigate();
-  const [emp, setEmp] = React.useState([]);
-
-  const getEmployees = async () => {
-    try {
-      const res = await axios.get("http://localhost:5000/employees", {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-      });
-      if (res.status === 200) {
-        setEmp(res.data);
-        console.log(res.data);
-      }
-      if (res.status === 401) {
-        console.log("looks like your token is expired");
-        navigate("/login");
-      }
-    } catch (err) {
-      console.log("looks like your token is expired");
-      navigate("/login");
-      console.log(err.response.status);
-    }
-  };
   return (
-    <div>
-      <button onClick={getEmployees}>Get Employees</button>
-    </div>
+    <Container>
+      <h1>Home</h1>
+      <UploadForm />
+      <Documents />
+    </Container>
   );
 };
 
