71a97a209e3cc0ee036e25dc425812197c016bcd
Shehab Shalan <shehab.shalan@hotmail.com>
2022-07-06 03:30:05 +0200

    user context updated to handle features

--- frontend/src/context/UserContext.js
+++ frontend/src/context/UserContext.js
@@ -5,35 +5,52 @@ import lodash from "lodash";
 const UserContext = createContext();
 
 export function UserContextProvider({ children }) {
-  const [files, setFiles] = React.useState([]);
+  const [filesUploaded, setFilesUploaded] = React.useState([]);
   const [expirationDateTime, setExpirationDateTime] = React.useState(
     new Date()
   );
-
   const [uploading, setUploading] = React.useState(false);
   const [uploadingError, setUploadingError] = React.useState(false);
   const [uploadingSuccess, setUploadingSuccess] = React.useState(false);
+  const [sharingError, setSharingError] = React.useState(false);
+  const [sharingSuccess, setSharingSuccess] = React.useState(false);
+  const [files, setFiles] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [open, setOpen] = useState(false);
+  const [generatingLink, setGeneratingLink] = useState(false);
+  const [sharingLink, setSharingLink] = useState("");
+  const [fileId, setFileId] = useState("");
+
+  const getFiles = async () => {
+    setLoading(true);
+    try {
+      const res = await axios.get("http://localhost:5000/files");
+      console.log(res.data);
+      setFiles(res.data);
+      setLoading(false);
+    } catch (err) {
+      setLoading(false);
+      console.log(err);
+    }
+  };
+
+  React.useEffect(() => {
+    getFiles();
+  }, []);
+
   const handleExpirationDateTime = (newValue) => {
     setExpirationDateTime(newValue._d.toISOString());
   };
   const handleFileChange = (e) => {
     const selectedFiles = [...e.target.files];
-    setFiles(selectedFiles);
-    const map = selectedFiles.map((file) => {
-      return {
-        fileName: file.name,
-        fileSizeInBytes: file.size,
-        fileType: file.type,
-      };
-    });
-    console.log(map);
+    setFilesUploaded(selectedFiles);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setUploading(true);
     const formData = new FormData();
-    lodash.forEach(files, (file) => {
+    lodash.forEach(filesUploaded, (file) => {
       formData.append("myfile", file);
     });
 
@@ -44,15 +61,53 @@ export function UserContextProvider({ children }) {
       .then((res) => {
         setUploading(false);
         setUploadingSuccess(true);
-        setFiles([]);
+        setFilesUploaded([]);
+        getFiles();
       })
       .catch((err) => {
         setUploading(false);
         setUploadingError(true);
-        setFiles([]);
+        setFilesUploaded([]);
       });
   };
 
+  const handleOpen = (id) => {
+    setOpen(true);
+    console.log(id);
+    setFileId(id);
+  };
+  const handleClose = () => setOpen(false);
+  const handleShareFile = async (e) => {
+    e.preventDefault();
+    setGeneratingLink(true);
+    const payload = {
+      fileId: fileId,
+      expiresAt: expirationDateTime,
+    };
+    console.log(payload);
+    try {
+      const res = await axios.post("http://localhost:5000/sharefile", payload);
+      console.log(res.data.message);
+      setSharingLink(res.data.message);
+      setGeneratingLink(false);
+      setSharingSuccess(true);
+      console.log(res.data);
+    } catch (err) {
+      setGeneratingLink(false);
+      setSharingError(true);
+      console.log(err);
+    }
+  };
+
+  const handleDownloads = async (id) => {
+    try {
+      const res = await axios.put(`http://localhost:5000/files/${id}`);
+      console.log(res.data);
+      getFiles();
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <UserContext.Provider
       value={{
@@ -60,10 +115,21 @@ export function UserContextProvider({ children }) {
         handleSubmit,
         handleExpirationDateTime,
         expirationDateTime,
-        files,
+        filesUploaded,
         uploading,
         uploadingError,
         uploadingSuccess,
+        files,
+        loading,
+        open,
+        generatingLink,
+        handleOpen,
+        handleClose,
+        handleShareFile,
+        sharingError,
+        sharingSuccess,
+        sharingLink,
+        handleDownloads,
       }}
     >
       {children}
