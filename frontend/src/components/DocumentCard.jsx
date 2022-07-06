50863633bbfff21ce704bebf3ccdd0643390823d
Shehab Shalan <shehab.shalan@hotmail.com>
2022-07-06 19:42:55 +0200

    removed documentIcon

--- frontend/src/components/DocumentCard.jsx
+++ frontend/src/components/DocumentCard.jsx
@@ -4,36 +4,16 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 
-import {
-  FaFileWord,
-  FaFileImage,
-  FaFileExcel,
-  FaFilePdf,
-  FaFileDownload,
-} from "react-icons/fa";
-import { RiFileTextFill } from "react-icons/ri";
+import { FaFileDownload } from "react-icons/fa";
 
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import Grid from "@mui/material/Grid";
 import { Button, CardActions, Divider } from "@mui/material";
 import { useUserContext } from "../context/UserContext";
+import documentIcon from "../utils/documentIcon";
 import toDateTime from "../utils/toDateTime";
 
-export const documentIcon = (fileType) => (
-  <>
-    {fileType === "doc" && <FaFileWord size={30} />}
-    {fileType === "docx" && <FaFileWord size={30} />}
-    {fileType === "pdf" && <FaFilePdf size={30} />}
-    {fileType === "xls" && <FaFileExcel size={30} />}
-    {fileType === "csv" && <FaFileExcel size={30} />}
-    {fileType === "txt" && <RiFileTextFill size={30} />}
-    {fileType === "jpg" && <FaFileImage size={30} />}
-    {fileType === "png" && <FaFileImage size={30} />}
-    {fileType === "gif" && <FaFileImage size={30} />}
-    {fileType === "svg" && <FaFileImage size={30} />}
-  </>
-);
 const DocumentCard = ({ file }) => {
   const { handleOpen, handleDownloads } = useUserContext();
   const supportedPreviewFileTypes = ["jpg", "png", "gif", "svg"];
@@ -73,11 +53,6 @@ const DocumentCard = ({ file }) => {
               />
             </ListItem>
           </Grid>
-          {/* <Grid item xs={4} md={4}>
-            <ListItem>
-              <ListItemText primary={file.fileSizeInMb} />
-            </ListItem>
-          </Grid> */}
           <Grid item xs={4} md={4}>
             <ListItem>
               <FaFileDownload size={20} />
@@ -87,7 +62,7 @@ const DocumentCard = ({ file }) => {
         </Grid>
         <Divider sx={{ my: 2 }} />
         <Typography variant="body1" gutterBottom>
-          uploaded in {toDateTime(file.createdAt)}
+          uploaded on {toDateTime(file.createdAt)}
         </Typography>
         <Divider />
       </CardContent>
