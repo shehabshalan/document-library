dd4cdc0c758f49661e7e0eabed27d3570a263296
Shehab Shalan <shehab.shalan@hotmail.com>
2022-07-06 03:28:49 +0200

    footer added

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+
+const Footer = () => {
+  const today = new Date();
+
+  return (
+    <Box component={"footer"}>
+      <Typography>
+        Copyright &copy; {today.getFullYear()} Document Sharing
+      </Typography>
+    </Box>
+  );
+};
+
+export default Footer;
