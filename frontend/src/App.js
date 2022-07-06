05938c292ba05628130dbaf114a91782d5890170
Shehab Shalan <shehab.shalan@hotmail.com>
2022-07-06 07:08:42 +0200

    moved navbar to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
-import UploadForm from "./components/UploadForm";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import ShareFile from "./pages/ShareFile";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
+      <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sharefile/:id" element={<ShareFile />} />
